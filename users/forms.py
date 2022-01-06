368797914188cf58ef8b782c4c59eb35d7bef55a
taye000 <taylorgitari@gmail.com>
2022-01-06 15:33:35 +0300

    mydjango update

--- users/forms.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-
-class CustomUserCreationForm(UserCreationForm):
-    class meta(UserCreationForm.meta):
-        fields = UserCreationForm.meta.fields + ("email",)
